ad93a7ecec68bfe12dc41ff6522ca0c0d9cad844
KolyanGrom <kolyangrom@yandex.ru>
2024-04-10 19:38:57 +0700

    add Headless

--- src/test/java/webSiteTests.java
+++ src/test/java/webSiteTests.java
@@ -7,19 +7,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class webSiteTests {
     private WebDriver driver;
+
     @BeforeAll
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
+
     @AfterEach
     void tearDown() {
         driver.quit();
